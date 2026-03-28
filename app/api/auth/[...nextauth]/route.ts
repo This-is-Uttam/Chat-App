bbd9794ad04c1abbd63101e7b50b071138539c64
Your Name <uttam592k@gmail.com>
2026-03-28 21:30:37 +0530

    next_auth callback bugs fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,36 +1,34 @@
 import { connectDB } from "@/lib/db";
 import { User } from "@/lib/models/User";
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const runtime = "nodejs";
 
-
-export const authOptions = {
-
+export const authOptions: NextAuthOptions = {
   session: {
-  strategy: "jwt" as const,
-},
-  
+    strategy: "jwt" as const,
+  },
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    })
+    }),
   ],
 
   callbacks: {
     async signIn({ user }) {
       await connectDB();
 
-      const existingUser = await User.findOne({email: user.email})
+      const existingUser = await User.findOne({ email: user.email });
 
-      if(!existingUser) {
+      if (!existingUser) {
         await User.create({
           name: user.name,
           email: user.email,
-          image: user.image
-        })
+          image: user.image,
+        });
       }
 
       return true;
@@ -38,7 +36,6 @@ export const authOptions = {
   },
 };
 
-const handler = NextAuth(authOptions)
-
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
