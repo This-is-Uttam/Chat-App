01b636f3231a2667a940f043072b32ddfc558513
Your Name <uttam592k@gmail.com>
2026-04-02 16:54:22 +0530

    loading UI fix

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -46,7 +46,7 @@ export default function ChatPage() {
   const [users, setUsers] = useState<TUser[]>([]);
   const [selectedUser, setSelectedUser] = useState<TUser>();
   const [isChatsLoading, setisChatsLoading] = useState(true);
-  const [isMsgsLoading, setisMsgsLoading] = useState(true);
+  const [isMsgsLoading, setisMsgsLoading] = useState(false);
   const [currentRoomId, setCurrentRoomId] = useState<string | null>(null);
   const bottomRef = useRef<HTMLDivElement | null>(null);
   const [lastMsgs, setLastMsgs] = useState<string[]>([]);
@@ -470,7 +470,12 @@ export default function ChatPage() {
 
             {/* Messages */}
             <div className="flex-1 overflow-y-auto  p-4">
-              {messages.length === 0 ? (
+              {isMsgsLoading ? (
+                <div className="flex justify-center items-center mt-10 gap-3">
+                  <Spinner />
+                  <div className="text-gray-800">Loading Conversations</div>
+                </div>
+              ) : messages.length === 0 ? (
                 <div className="h-[90vh] text-gray-800 flex justify-center items-center text-center">
                   No messages yet. <br /> Click a user to start the
                   conversation!
@@ -479,31 +484,24 @@ export default function ChatPage() {
                 <div className="flex flex-col">
                   {/* Conversations */}
                   <div>
-                    {isMsgsLoading ? (
-                      <div className="flex justify-center items-center mt-10 gap-3">
-                        <Spinner />
-                        <div className="text-gray-800">Loading Conversations</div>
-                      </div>
-                    ) : (
-                      messages.map((msg, index) => (
+                    {messages.map((msg, index) => (
+                      <div
+                        key={index}
+                        className={`max-w-[70%] w-fit px-3 my-1 rounded-lg flex gap-3 justify-between ${
+                          msg.sender === session?.user?.email
+                            ? "ml-auto bg-[#3f0497] text-white"
+                            : "bg-white"
+                        }`}
+                      >
+                        <div className="py-1">{msg.message}</div>
                         <div
-                          key={index}
-                          className={`max-w-[70%] w-fit px-3 my-1 rounded-lg flex gap-3 justify-between ${
-                            msg.sender === session?.user?.email
-                              ? "ml-auto bg-[#3f0497] text-white"
-                              : "bg-white"
-                          }`}
-                        >
-                          <div className="py-1">{msg.message}</div>
-                          <div
-                            className={` text-xs shrink-0 text-right flex items-end pb-1
+                          className={` text-xs shrink-0 text-right flex items-end pb-1
                    mt-1 ${msg.sender === session?.user?.email ? "text-blue-200" : "text-gray-500"}`}
-                          >
-                            {formatTime(msg.createdAt)}
-                          </div>
+                        >
+                          {formatTime(msg.createdAt)}
                         </div>
-                      ))
-                    )}
+                      </div>
+                    ))}
                   </div>
                 </div>
               )}
